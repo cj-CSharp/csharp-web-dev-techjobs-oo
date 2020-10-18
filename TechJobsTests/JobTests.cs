59ca62feb5b9f836ba2e9cae85890e30b43449c0
cjallen1988 <cjallen1988@gmail.com>
2020-10-18 17:45:00 -0500

    Test full constructor for Job Class

---
+++ TechJobsTests/JobTests.cs
@@ -0,0 +1,28 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechJobsOO;
+
+namespace TechJobsTests
+{
+    [TestClass]
+    public class JobTests
+    {
+        [TestMethod]
+        public void TestSettingJobId()
+        {
+            Job job1 = new Job();
+            Job job2 = new Job();
+            Assert.AreEqual(1, job2.Id - job1.Id);
+        }
+
+        [TestMethod]
+        public void TestJobConstructionSetsAllFields ()
+        {
+            Job job1 = new Job("Product Tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"),new CoreCompetency( "Persistence"));
+            Assert.AreEqual("Product Tester", job1.Name);
+            Assert.AreEqual("ACME", job1.EmployerName.Value);
+            Assert.AreEqual("Desert", job1.EmployerLocation.Value);
+            Assert.AreEqual("Quality control", job1.JobType.Value);
+            Assert.AreEqual("Persistence", job1.JobCoreCompetency.Value);
+        }
+    }
+}
